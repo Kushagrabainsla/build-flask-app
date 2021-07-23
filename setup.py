f2998bf8dc17e84c2ccb3417860e83cb08c16177
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-07-23 18:01:00 +0530

    Updates

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='build-flask-app',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.3',
+    license='MIT',
     author='Kushagra Bainsla',
     author_email='kushagrabainsla@gmail.com',
+    url='https://github.com/Kushagrabainsla/build-flask-app',
     description = 'The build-flask-app is an excellent tool for beginners, which allows you to create and run Flask projects very quickly',
-    url="https://github.com/Kushagrabainsla/create-flask-app",
     install_requires=['PyInquirer'],
     entry_points = {
         'console_scripts': ['build-flask-app=build_flask_app.main:main'],
