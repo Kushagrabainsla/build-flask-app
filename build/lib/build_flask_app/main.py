297d6c60f736e2020256d24e779010241f323e3c
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-07-23 18:12:52 +0530

    Updates

--- build/lib/build_flask_app/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from build_flask_app.Components import StartProcess
-
-def main():
-    if len(sys.argv) == 1: return StartProcess()
-    for arg in sys.argv:
-        if arg == '-v' or arg == '--version':
-            print('Version 0.0.1')
-    
